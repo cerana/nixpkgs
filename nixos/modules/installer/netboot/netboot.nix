6d5bb878374ad913f34324a38bb1dee88df695d0
Nahum Shalman <nshalman@omniti.com>
2016-05-10 20:17:32 +0000

    ship gdb and strace

--- nixos/modules/installer/netboot/netboot.nix
+++ nixos/modules/installer/netboot/netboot.nix
@@ -17,7 +17,7 @@ with lib;
 
     # !!! Hack - attributes expected by other modules.
     system.boot.loader.kernelFile = "bzImage";
-    environment.systemPackages = [ pkgs.grub2 pkgs.libselinux pkgs.qemu_kvm ];
+    environment.systemPackages = [ pkgs.grub2 pkgs.libselinux pkgs.qemu_kvm pkgs.strace pkgs.gdb ];
 
     boot.consoleLogLevel = mkDefault 7;
 
