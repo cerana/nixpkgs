8aa3e2acbc3076621619a7a719e010810fe7ba5b
Nahum Shalman <nshalman@omniti.com>
2016-05-13 13:40:15 +0000

    ship consul (example golang application)

--- nixos/modules/installer/netboot/netboot.nix
+++ nixos/modules/installer/netboot/netboot.nix
@@ -17,7 +17,7 @@ with lib;
 
     # !!! Hack - attributes expected by other modules.
     system.boot.loader.kernelFile = "bzImage";
-    environment.systemPackages = [ pkgs.grub2 pkgs.libselinux pkgs.qemu_kvm pkgs.strace pkgs.gdb pkgs.lshw];
+    environment.systemPackages = [ pkgs.grub2 pkgs.libselinux pkgs.qemu_kvm pkgs.strace pkgs.gdb pkgs.lshw pkgs.consul ];
 
     boot.consoleLogLevel = mkDefault 7;
 
