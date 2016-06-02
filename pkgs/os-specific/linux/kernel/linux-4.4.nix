c87715d33b0370eb1b20969e42c340865094906d
Nahum Shalman <nshalman@omniti.com>
2016-06-02 17:06:57 +0000

    TEMPORARY HACK: Use unmodified ZFS and SPL
    This reverts commit f72b76a9e8d51e3dc30a8a856da8c9603b41c08f.
    This reverts commit 2ce19b5b69ec8353ead47882a876deb7d1971432.

--- pkgs/os-specific/linux/kernel/linux-4.4.nix
+++ pkgs/os-specific/linux/kernel/linux-4.4.nix
@@ -9,7 +9,7 @@ import ./generic.nix (args // rec {
     sha256 = "18v4n7yypl4c8k69zrnf9g09pilh47y0ciy3mwbksz2kmw4yq573";
   };
 
-  kernelPatches = args.kernelPatches ++ [ { name = "datasetns"; patch = ./datasetns.patch; } ];
+  kernelPatches = args.kernelPatches;
 
   features.iwlwifi = true;
   features.efiBootStub = true;
