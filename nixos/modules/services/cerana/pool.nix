b8a0525a971119917bb51e9629dacec037ed4f87
Nahum Shalman <nshalman@omniti.com>
2016-06-03 16:32:05 +0000

    ignore swap

--- nixos/modules/services/cerana/pool.nix
+++ nixos/modules/services/cerana/pool.nix
@@ -12,8 +12,8 @@ in
     systemd.services.ceranapool = {
       description = "CeranaPool";
       path = [ pkgs.cerana-scripts ];
-      requiredBy = [ "network-pre.target" "multi-user.target" "sysinit.target" "swap.target" "local-fs-pre.target" "zfs-import.target" "systemd-journald.service" "basic.target" "ntpd.service" "sshd.service" "time-sync.service" "getty.target" "ip-up.target" ];
-      before = [ "sshd.service" "ntpd.service" "swap.target" "local-fs.target" ];
+      requiredBy = [ "network-pre.target" "multi-user.target" "sysinit.target" "local-fs-pre.target" "zfs-import.target" "systemd-journald.service" "basic.target" "ntpd.service" "sshd.service" "time-sync.service" "getty.target" "ip-up.target" ];
+      before = [ "sshd.service" "ntpd.service" "local-fs.target" ];
       serviceConfig = {
         Type = "oneshot";
         ExecStart = "${pkgs.cerana-scripts}/scripts/init-zpools.sh";
