fe681c09b30baa21e566008227a683bd14338d50
Nahum Shalman <nshalman@omniti.com>
2016-06-07 13:52:48 +0000

    early version of network initialization

--- nixos/modules/module-list.nix
+++ nixos/modules/module-list.nix
@@ -123,6 +123,7 @@
   ./services/backup/znapzend.nix
   ./services/cerana/coordinator.nix
   ./services/cerana/metrics-provider.nix
+  ./services/cerana/network.nix
   ./services/cerana/systemd-provider.nix
   ./services/cerana/zfs-provider.nix
   ./services/cerana/pool.nix
