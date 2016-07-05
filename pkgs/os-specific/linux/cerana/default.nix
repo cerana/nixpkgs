90909e11b505ab8fe692e7da3472a0e17a883848
Nahum Shalman <nshalman@omniti.com>
2016-07-05 14:34:41 +0000

    cerana scripts version bump

--- pkgs/os-specific/linux/cerana/default.nix
+++ pkgs/os-specific/linux/cerana/default.nix
@@ -2,7 +2,7 @@
 
 buildGoPackage rec {
   name = "cerana-${version}";
-     version = "2016-07-01";
+     version = "2016-07-05";
      owner = "cerana";
      repo = "cerana";
      rev = "187_boot_time_config";
@@ -13,7 +13,7 @@ buildGoPackage rec {
     owner = "cerana";
     repo = "cerana";
     inherit rev;
-    sha256 = "1rbhrwk9x8rpr8il8znrx9jnmpqd6wbhgdy5mq0b1wl447wpf6rl";
+    sha256 = "1mdhy7lyrdp80nnhxpq644h66am23xa4ww7689jlf3dcfpdn0vwb";
   };
 
   preConfigure = ''
