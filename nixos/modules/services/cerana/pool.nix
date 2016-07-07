5203bf1acade518d1423657a22aea9255a70fc7b
Steve Isaacs <sisaacs@omniti.com>
2016-07-07 17:52:26 -0400

    Added a oneshot unit file to copy the log directory from the ramdisk (/var/log) to the zpool (/data) and rotate logs from previous boots. A symlink is used to point the log file to the correct location and journald is restarted to use the new location. Each reboot rotates the log to a max of 4 previous reboots.

--- nixos/modules/services/cerana/pool.nix
+++ nixos/modules/services/cerana/pool.nix
@@ -7,13 +7,18 @@ let
 in
 {
   options.services.ceranapool.enable = mkEnableOption "Cerana Pool Configuration";
-  
+
   config = mkIf cfg.enable {
     systemd.services.ceranapool = {
       description = "Cerana Pool Configuration";
-      path = [ pkgs.cerana-scripts pkgs.gawk pkgs.utillinux pkgs.gnugrep pkgs.grub2 pkgs.gptfdisk pkgs.systemd pkgs.zfs pkgs.bash pkgs.coreutils];
-      requiredBy = [ "network-pre.target" "multi-user.target" "sysinit.target" "local-fs-pre.target" "zfs-import.target" "systemd-journald.service" "basic.target" "ntpd.service" "sshd.service" "time-sync.service" "getty.target" "ip-up.target" ];
-      before = [ "sshd.service" "ntpd.service" "local-fs.target" ];
+      path = [ pkgs.cerana-scripts pkgs.gawk pkgs.utillinux pkgs.gnugrep
+        pkgs.grub2 pkgs.gptfdisk pkgs.systemd pkgs.zfs pkgs.bash pkgs.coreutils];
+      requiredBy = [ "network-pre.target" "multi-user.target" "sysinit.target"
+        "local-fs-pre.target" "zfs-import.target" "systemd-journald.service"
+        "basic.target" "ntpd.service" "sshd.service" "time-sync.service"
+        "getty.target" "ip-up.target" "ceranaMoveLogs.service"];
+      before = [ "sshd.service" "ntpd.service" "local-fs.target"
+        "ceranaMoveLogs.service"];
       serviceConfig = {
         Type = "oneshot";
         ExecStart = "${pkgs.cerana-scripts}/scripts/init-zpools.sh";
