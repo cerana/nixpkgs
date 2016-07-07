6033676c0f7fca084ce82002aa80d7d386e079fd
Nahum Shalman <nshalman@omniti.com>
2016-07-07 19:16:00 +0000

    ship patchelf

--- nixos/modules/installer/netboot/netboot.nix
+++ nixos/modules/installer/netboot/netboot.nix
@@ -18,7 +18,7 @@ with lib;
 
     # !!! Hack - attributes expected by other modules.
     system.boot.loader.kernelFile = "bzImage";
-    environment.systemPackages = [ pkgs.grub2 pkgs.libselinux pkgs.qemu_kvm pkgs.strace pkgs.gdb pkgs.lshw pkgs.consul.bin pkgs.cerana.bin pkgs.cerana-scripts pkgs.dhcpcd pkgs.gptfdisk pkgs.glide pkgs.go_1_6 pkgs.git pkgs.vim ];
+    environment.systemPackages = [ pkgs.grub2 pkgs.libselinux pkgs.qemu_kvm pkgs.strace pkgs.gdb pkgs.lshw pkgs.consul.bin pkgs.cerana.bin pkgs.cerana-scripts pkgs.dhcpcd pkgs.gptfdisk pkgs.glide pkgs.go_1_6 pkgs.git pkgs.vim pkgs.patchelf ];
 
     fileSystems."/" =
       { fsType = "tmpfs";
