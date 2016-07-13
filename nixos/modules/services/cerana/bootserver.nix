8b865606de36726df0fcc247a18c89f3ebe6f11c
Steve Isaacs <sisaacs@omniti.com>
2016-07-13 16:16:07 -0400

    Added the bootserver service unit file.

---
+++ nixos/modules/services/cerana/bootserver.nix
@@ -0,0 +1,44 @@
+{ config, lib, pkgs, ... }:
+
+with lib;
+
+let
+  cfg = config.services.ceranaBootserver;
+  name = "bootserver";
+  cfgdir = "/data/config/";
+  cfgfile = "bootserver.json";
+  socketdir = "/task-socket/l2-coordinator/";
+  socket = "coordinator/l2-coord.sock";
+  daemon = "${pkgs.cerana.bin}/bin/bootserver";
+in
+{
+  options.services.ceranaBootserver.enable = mkEnableOption "ceranaBootserver";
+
+  config = mkIf cfg.enable {
+    systemd.services.ceranaBootserver = {
+      description = "Cerana PXE Boot Server";
+      wantedBy = [ "ceranaLayer2.target" ];
+      after = [ "ceranaL2Coordinator.service" ];
+      serviceConfig = {
+        Type = "simple";
+        Restart = "always";
+        ExecStart = "${daemon} -c ${cfgdir}${cfgfile}";
+      };
+      preStart = ''
+        if [ ! -f ${cfgdir}${cfgfile} ]; then
+                echo "{" > ${cfgdir}${cfgfile}
+                echo '  "service_name": "${name}",' >> ${cfgdir}${cfgfile}
+                echo '  "socket_dir": "${socketdir}",' >> ${cfgdir}${cfgfile}
+                echo '  "coordinator_url": "unix://${socketdir}${socket}",' >> ${cfgdir}${cfgfile}
+                echo '  "dhcp_interface": "mgmt0",' >> ${cfgdir}${cfgfile}
+                echo '  "ipxe": "/tmp/undionly.kpxe",' >> ${cfgdir}${cfgfile}
+                # Can use symlinks for these which may help with updates.
+                # Actually, "current" is a symlink.
+                echo '  "initrd": "/data/platform/current/initrd",' >> ${cfgdir}${cfgfile}
+                echo '  "kernel": "/data/platform/current/bzImage"' >> ${cfgdir}${cfgfile}
+                echo "}" >> ${cfgdir}${cfgfile}
+        fi
+        '';
+    };
+  };
+}
