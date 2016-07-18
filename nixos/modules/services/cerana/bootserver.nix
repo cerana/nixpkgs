1bfe97b0e53cdcd0f8390e0ef1bd6b891005ee35
Nahum Shalman <nshalman@omniti.com>
2016-07-18 16:51:13 +0000

    fix bootserver unit file

--- nixos/modules/services/cerana/bootserver.nix
+++ nixos/modules/services/cerana/bootserver.nix
@@ -30,7 +30,7 @@ in
                 echo '  "service_name": "${name}",' >> ${cfgdir}${cfgfile}
                 echo '  "socket_dir": "${socketdir}",' >> ${cfgdir}${cfgfile}
                 echo '  "coordinator_url": "unix://${socketdir}${socket}",' >> ${cfgdir}${cfgfile}
-                echo '  "dhcp_interface": "mgmt0",' >> ${cfgdir}${cfgfile}
+                echo '  "dhcp-interface": "mgmt0",' >> ${cfgdir}${cfgfile}
                 echo '  "ipxe": "${pkgs.ipxe}/undionly.kpxe",' >> ${cfgdir}${cfgfile}
                 # Can use symlinks for these which may help with updates.
                 # Actually, "current" is a symlink.
