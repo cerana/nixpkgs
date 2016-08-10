8e28c114db5062ca796d24c0eef21a3a0de64277
Nahum Shalman <nshalman@omniti.com>
2016-08-10 15:59:30 +0000

    build a demo-capable build of CeranaOS

--- pkgs/os-specific/linux/cerana/default.nix
+++ pkgs/os-specific/linux/cerana/default.nix
@@ -5,7 +5,7 @@ buildGoPackage rec {
      version = "2016-08-10";
      owner = "cerana";
      repo = "cerana";
-     rev = "d4ce56ca6b6667f825670f38d9e21dec84b45144";
+     rev = "8a448b1d8c1827ef6df75191a3a2b17eb70c8d18";
 
   goPackagePath = "github.com/cerana/cerana";
 
@@ -13,7 +13,7 @@ buildGoPackage rec {
     owner = "cerana";
     repo = "cerana";
     inherit rev;
-    sha256 = "1l8s0yb1dffynz3wyq68ixk6fz3xdi0hgxg6k0nxip293774nfn3";
+    sha256 = "0miyzb9n4yarblk8bgjmkm0h4p86kbnfiigz06151id2z8cvf2y8";
   };
 
   preConfigure = ''
